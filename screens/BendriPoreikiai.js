8bcf45f70962770f750ebdb34663f54ab6e28ae3
Paulius <paulius.gaizauskas@gmail.com>
2020-06-19 08:04:01 +0300

    Add loading based on settings

--- screens/BendriPoreikiai.js
+++ screens/BendriPoreikiai.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { SafeAreaView, View, ScrollView, StyleSheet, Text } from 'react-native';
+
+import {NEEDS_COMMON_IEVA, NEEDS_COMMON_ROL} from "../data";
 
 import { globalStyles } from "./styles";
 import {
@@ -7,17 +9,7 @@ import {
   CollapseHeader,
   CollapseBody,
 } from "accordion-collapse-react-native";
-
-const NEEDS = [
-    { title: 'Fizinė gerovė', elements: ['Oras', 'Šviesa', 'Mityba', 'Vanduo', 'Šiluma', 'Poilsis', 'Miegas', 'Sveikata', 'Pastogė', 'Prieglobstis', 'Judėjimas', 'Fiziniai pratimai' ]  },
-    { title: 'Prisilietimas', elements: ['Seksualinė išraiška', 'Saugumas', 'Komfortas', 'Emocinis saugumas', 'Apsauga nuo skausmo', 'Išsaugojimas', 'Tausojimas' ]  },
-    { title: 'Harmonija', elements: ['Taika', 'Grožis', 'Ramybė', 'Rimtis', 'Pusiausvyra', 'Atsipalaidavimas', 'Tyla', 'Tvarka', 'Lengvumas', 'Tvarumas', 'Darna', 'Kongruencija', 'Lygybė', 'Balansas', 'Stabilumas' ,'Integracija', 'Suvokimas', 'Vientisumas', 'Užbaigtumas', 'Nuspėjamumas', 'Familiarumas', 'Teisingumas', 'Sąžiningumas'  ]  },
-    { title: 'Ryšys', elements: ['Meilė', 'Meilė sau', 'Rūpestis', 'Rūpestis savimi', 'Priklausymas', 'Artumas', 'Intymumas', 'Prieraišumas', 'Empatija', 'Atjauta', 'Dėkingumas', 'Įvertinimas', 'Pripažinimas', 'Priėmimas', 'Nuraminimas', 'Dėmesys', 'Atvirumas', 'Pasitikėjimas', 'Komunikacija', 'Pagarba', 'Dalijimasis', 'Mainai', 'Davimas', 'Gavimas', 'Švelnumas', 'Meilumas' , 'Jautrumas' , 'Gerumas', 'Matyti ir būti matomu', 'Girdėti ir būti išgirstu', 'Suprasti ir būti suprastu', 'Mano poreikių pripažinimas', 'Atidumas', 'Puoselėjimas', 'Dalyvavimas', 'Įsitraukimas', 'Parama', 'Pagalba', 'Savitarpiškumas', 'Bendradarbiavimas', 'Partnerystė', 'Bendrystė', 'Bičiulystė', 'Draugystė', 'Abipusiškumas', 'Nuoseklumas', 'Tęstinumas'  ]  },
-    { title: 'Prasmė', elements: ['Gyvenimo praturtinimas', 'Buvimas', 'Susitelkimas', 'Ryšys su savimi', 'Susikaupimas', 'Viltis', 'Vizija', 'Svajojimas', 'Tikėjimas', 'Aiškumas', 'Žinojimas (realybės matymas)', 'Mokymasis', 'Suvokimas', 'Sąmoningumas', 'Paprastumas', 'Įkvėpimas', 'Kūrybiškumas', 'Iššūkis', 'Stimuliacija', 'Augimas', 'Evoliucija', 'Progresas', 'Pažanga', 'Plėtra', 'Vystymas', 'Tyrinėjimas', 'Jėga', '(Vidinė) Stiprybė', 'Kompetencija', 'Gebėjimas', 'Įgalinimas', 'Savivertė', 'Orumas', 'Pasitikėjimas savimi', 'Savigarba', 'Efektyvumas', 'Veiksmingumas', 'Rūpėti', 'Dalyvauti', 'Tikslas', 'Turėti savo vietą pasaulyje', 'Dvasingumas', 'Išsilaisvinimas', 'Transformacija', 'Virsmas', 'Tarpusavio susiejimas', 'Šventimas', 'Gedėjimas'  ]  },
-    { title: 'Atvirumas', elements: ['Autentiškumas', 'Saviraiška', 'Vientisumas', 'Skaidrumas', 'Tikrumas', 'Tiesa']  },
-    { title: 'Žaidimas', elements: ['Gyvumas', 'Žvalumas', 'Gyvybingumas', 'Judrumas', 'Tekmė', 'Aistra', 'Linksmybės', 'Spontaniškumas', 'Lengvumas', 'Humoras', 'Juokas', 'Įvairovė', 'Atradimas', 'Nuotykis', 'Atsinaujinimas', 'Atgaiva']  },
-  ];
-
+import AsyncStorage from "@react-native-community/async-storage";
 
 
 
@@ -48,10 +40,38 @@ const NEEDS = [
   );
 
 export default function BendriPoreikiai() {
+  const [sortBy, setSortBy] = useState();
+
+
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@ordering");
+      if (value !== null) {
+        setSortBy(value);
+      } else {
+        setSortBy('empatijos-bendruomene'); //default value
+      }
+
+
+    } catch (e) {
+      console.log(e)
+    }
+  };
+
+
+  useEffect(() => {
+    getData();
+    if(__DEV__){
+      console.log(sortBy)
+    }
+
+  }, [sortBy]);
+
     return (
       <View style={globalStyles.container}>
+      {__DEV__ ? <Text>sorting by: {sortBy}</Text>: null}
       <ScrollView>
-             {NEEDS.map(renderItem)}
+          {sortBy === 'empatijos-bendruomene' ? NEEDS_COMMON_ROL.map(renderItem) : NEEDS_COMMON_IEVA.map(renderItem)}
         </ScrollView>
         </View>
       );

60978904e2195018d52172f608160c91cbd3c75b
Paulius <paulius.gaizauskas@gmail.com>
2020-06-20 06:38:29 +0300

    Add needs from cnvc

--- data.js
+++ data.js
@@ -73,7 +73,6 @@ export const NEEDS_UNMET_ROL = [
       ];
 
 
-      // TODO
 export const NEEDS_COMMON_ROL= [
         { title: 'Ryšys', elements: ['Priėmimas', 'Įsitraukimas', 'Pritarimas', 'Švelnumas', 'Įvertinimas', 'Priklausymas', 'Bendrumas', 'Rūpinimasis', 'Atidumas', 'Globa', 'Artumas', 'Komunikacija', 'Bendravimas', 'Bendruomenė', 'Draugija', 'Draugystė', 'Atjauta', 'Užuojauta', 'Dėmesys', 'Empatija', 'Įkvėpimas', 'Vientisumas', 'Intymumas', 'Meilė', 'Šiluma', 'Abipusiškumas', 'Skatinimas', 'Puoselėjimas', 'Partnerystė', 'Buvimas kartu', 'Pagarba', 'Savigarba', 'Savęs priėmimas', 'Rūpinimasis savimi', 'Ryšys su savimi', 'Savęs išreiškimas', 'Bendra tikrovė', 'Dalinimasis tikrove', 'Stabilumas', 'Palaikymas', 'Pažinti', 'Būti pažintam', 'Pastebėti', 'Būti pastebėtam', 'Pasitikėjimas', 'Supratimas' ]  },
         { title: 'Prasmė', elements: ['Sąmoningumas', 'Šventimas', 'Iššūkis', 'Kūrybiškumas', 'Aiškumas', 'Skaidrumas', 'Sugebėjimas', 'Savimonė', 'Prisidėjimas', 'Atradimas', 'Naudingumas', 'Veiksmingumas', 'Produktyvumas', 'Pasisekimas', 'Augimas', 'Mokymasis', 'Gedėjimas', 'Veiksmas', 'Dalyvavimas', 'Požiūris', 'Perspektyva', 'Galimybė', 'Pažanga', 'Progresas', 'Tikslas', 'Stimulas', 'Tikėjimas' ]  },
@@ -83,3 +82,45 @@ export const NEEDS_COMMON_ROL= [
         { title: 'Autonomija', elements: ['Pasirinkimas', 'Orumas', 'Nepriklausomybė', 'Saviraiška', 'Erdvė', 'Spontaniškumas' ]  },
         { title: 'Žaidimas', elements: ['Nuotykis', 'Susijaudinimas', 'Entuziazmas', 'Linksmumas', 'Smagumas', 'Humoras', 'Džiaugsmas', 'Atsipalaidavimas', 'Stimuliavimas' ]  },
       ];
+
+// by https://www.cnvc.org/
+
+export const NEEDS_COMMON_CNVC = [
+  { title: 'Connection', elements: ['Acceptance', 'Affection', 'Appreciation', 'Belonging', 'Cooperation', 'Communication', 'Closeness', 'Community', 'Companionship', 'Compassion', 'Consideration', 'Consistency', 'Empathy', 'Inclusion', 'Intimacy', 'Love', 'Mutuality', 'Nurturing', 'Respect/self-respect', 'Safety', 'Security', 'Stability', 'Support', 'To know and be known', 'To see and be seen', 'To understand and be understood', 'Trust', 'Warmth'  ]  },
+  { title: 'Physical Well-Being', elements: ['Air', 'Food', 'Movement/Exercise', 'Rest/Sleep', 'Sexual expression', 'Safety', 'Shelter', 'Touch', 'Water' ]  },
+  { title: 'Honesty', elements: ['Authenticity', 'Integrity', 'Presence' ]  },
+  { title: 'Play', elements: ['Joy', 'Humor']  },
+  { title: 'Peace', elements: ['Beauty', 'Communion', 'Ease', 'Equality', 'Harmony', 'Inspiration', 'Order']  },
+  { title: 'Autonomy', elements: ['Choice', 'Freedom', 'Independence', 'Space', 'Spontaneity']  },
+  { title: 'Meaning', elements: ['Awareness', 'Celebration of life', 'Challenge', 'Clarity', 'Competence', 'Consciousness', 'Contribution', 'Creativity', 'Discovery', 'Efficacy', 'Effectiveness', 'Growth', 'Hope', 'Learning', 'Mourning', 'Participation', 'Purpose', 'Self-expression', 'Stimulation', 'To matter', 'Understanding']  },
+];
+
+export const NEEDS_MET_CNVC = [
+  { needs:"met", title: 'Affectionate', elements: ['Compassionate', 'Friendly', 'Loving', 'Open hearted', 'Sympathetic', 'Tender', 'Warm' ]  },
+  { needs:"met", title: 'Engaged', elements: ['Absorbed', 'Alert', 'Curious', 'Engrossed', 'Enchanted', 'Entranced', 'Fascinated', 'Interested', 'Intrigued', 'Involved', 'Spellbound', 'Stimulated' ]  },
+  { needs:"met", title: 'Hopeful', elements: ['Expectant', 'Encouraged', 'Optimistic',  ]  },
+  { needs:"met", title: 'Confident', elements: ['Empowered', 'Open', 'Proud', 'Safe', 'Secure'  ]  },
+  { needs:"met", title: 'Excited', elements: ['Amazed', 'Animated', 'Ardent', 'Aroused', 'Astonished', 'Dazzled', 'Eager', 'Energetic', 'Enthusiastic', 'Giddy', 'Invigorated', 'Lively', 'Passionate', 'Surprised', 'Vibrant'  ]  },
+  { needs:"met", title: 'Grateful', elements: ['Appreciative', 'Moved', 'Thankful', 'Touched' ]  },
+  { needs:"met", title: 'Inspired', elements: ['Amazed', 'Awed', 'Wonder' ]  },
+  { needs:"met", title: 'Joyful', elements: ['Amused', 'Delighted', 'Glad', 'Happy', 'Jubilant', 'Pleased', 'Tickled' ]  },
+  { needs:"met", title: 'Exhilarated', elements: ['Blissful', 'Ecstatic', 'Elated', 'Enthralled', 'Exuberant', 'Radiant', 'Rapturous', 'Thrilled' ]  },
+  { needs:"met", title: 'Peaceful', elements: ['Calm', 'Clear headed', 'Comfortable', 'Centered', 'Content', 'Equanimous', 'Fulfilled', 'Mellow', 'Quiet', 'Relaxed', 'Relieved', 'Satisfied', 'Serene', 'Still', 'Tranquil', 'Trusting' ]  },
+  { needs:"met", title: 'Refreshed', elements: ['Enlivened', 'Rejuvenated', 'Renewed', 'Rested', 'Restored', 'Revived' ]  },
+];
+export const NEEDS_UNMET_CNVC  = [
+  { needs:"unmet", title: 'Afraid', elements: ['Apprehensive', 'Dread', 'Foreboding', 'Frightened', 'Mistrustful', 'Panicked', 'Petrified', 'Scared', 'Suspicious', 'Terrified', 'Wary', 'Worried'  ]  },
+  { needs:"unmet", title: 'Annoyed', elements: ['Aggravated', 'Dismayed', 'Disgruntled', 'Displeased', 'Exasperated', 'Frustrated', 'Impatient', 'Irritated', 'Irked'  ]  },
+  { needs:"unmet", title: 'Angry', elements: ['Enraged', 'Furious', 'Incensed', 'Indignant', 'Irate', 'Livid', 'Outraged', 'Resentful' ]  },
+  { needs:"unmet", title: 'Aversion', elements: ['Animosity', 'Appalled', 'Contempt', 'Disgusted', 'Dislike', 'Hate', 'Horrified', 'Hostile', 'Repulsed' ]  },
+  { needs:"unmet", title: 'Confused', elements: ['Ambivalent', 'Baffled', 'Bewildered', 'Dazed', 'Hesitant', 'Lost', 'Mystified', 'Perplexed', 'Puzzled', 'Torn' ]  },
+  { needs:"unmet", title: 'Disconnected', elements: ['Alienated', 'Aloof', 'Apathetic', 'Bored', 'Cold', 'Detached', 'Distant', 'Distracted', 'Indifferent', 'Numb', 'Removed', 'Uninterested', 'Withdrawn' ]  },
+  { needs:"unmet", title: 'Disquiet', elements: ['Agitated', 'Alarmed', 'Discombobulated', 'Disconcerted', 'Disturbed', 'Perturbed', 'Rattled', 'Restless', 'Shocked', 'Startled', 'Surprised', 'Troubled', 'Turbulent', 'Turmoil', 'Uncomfortable', 'Uneasy', 'Unnerved', 'Unsettled', 'Upset' ]  },
+  { needs:"unmet", title: 'Embarrassed', elements: ['Ashamed', 'Chagrined', 'Flustered', 'Guilty', 'Mortified', 'Self-conscious' ]  },
+  { needs:"unmet", title: 'Fatigue', elements: ['Beat', 'Burnt out', 'Depleted', 'Exhausted', 'Lethargic', 'Listless', 'Sleepy', 'Tired', 'Weary', 'Worn out' ]  },
+  { needs:"unmet", title: 'Pain', elements: ['Agony', 'Anguished', 'Bereaved', 'Devastated', 'Grief', 'Heartbroken', 'Hurt', 'Lonely', 'Miserable', 'Regretful', 'Remorseful' ]  },
+  { needs:"unmet", title: 'Sad', elements: ['Depressed', 'Dejected', 'Despair', 'Despondent', 'Disappointed', 'Discouraged', 'Disheartened', 'Forlorn', 'Gloomy', 'Heavy hearted', 'Hopeless', 'Melancholy', 'Unhappy', 'Wretched' ]  },
+  { needs:"unmet", title: 'Tense', elements: ['Anxious', 'Cranky', 'Distressed', 'Distraught', 'Edgy', 'Fidgety', 'Frazzled', 'Irritable', 'Jittery', 'Nervous', 'Overwhelmed', 'Restless', 'Stressed out']  },
+  { needs:"unmet", title: 'Vulnerable', elements: ['Fragile', 'Guarded', 'Helpless', 'Insecure', 'Leery', 'Reserved', 'Sensitive', 'Shaky']  },
+  { needs:"unmet", title: 'Yearning', elements: ['Envious', 'Jealous', 'Longing', 'Nostalgic', 'Pining', 'Wistful']  },
+];

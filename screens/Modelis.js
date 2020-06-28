d7cf655761fb36feea480d723471fd4cefc9c8f1
Paulius <paulius.gaizauskas@gmail.com>
2020-06-28 07:58:26 +0300

    Add Blokai Modal text

--- screens/Modelis.js
+++ screens/Modelis.js
@@ -9,33 +9,14 @@ import {
 } from "@expo/vector-icons";
 import { globalStyles } from "./styles";
 import Card from "../shared/Card";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function Modelis() {
+const Tab = createBottomTabNavigator();
+
+function NVCModel() {
   const [modalOpen, setModalOpen] = useState(false);
   const [modalText, setModalText] = useState("");
-
-  //   const data = [
-  //     {
-  //       title: "Pastebėjimas",
-  //       description:
-  //         "Kas įvyko, be prielaidų ar interpretacijų - kitaip tariant realybės suvokimas per pojūčius. Kokia buvo vaizdinė, garsinė, jausminė, skonio ar lytėjimo informacija.",
-  //     },
-  //     {
-  //         title: "Jausmas",
-  //         description:
-  //         "Atsakas/reakciją į tai, kas įvyko, kurią pavadinkime judančia energija emociniame kūne "
-  //       },
-  //       {
-  //         title: "Poreikis",
-  //         description:
-  //         "Vidiniai norai, troškimai, kurie siekia pasireikšti ar būti pripažintais - kitaip tariant mūsų vidinės priežastys veikti."
-  //       },
-  //       {
-  //         title: "Prašymas",
-  //         description:
-  //         "Pasiūlymas imtis konkrečių veiksmų, kurie patenkintų svarbius poreikius."
-  //       },
-  //   ];
 
   return (
     <View style={globalStyles.container}>
@@ -133,6 +114,157 @@ export default function Modelis() {
         </Card>
       </TouchableOpacity>
     </View>
+  );
+}
+
+function Blocks() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalText, setModalText] = useState("");
+  return (
+    <View style={globalStyles.container}>
+      <Modal visible={modalOpen} animationType="slide" transparent={true}>
+        <View style={{ ...globalStyles.container, ...{} }}>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              // backgroundColor: 'blue'
+            }}
+            onPress={() => setModalOpen(false)}
+          ></TouchableOpacity>
+
+          <View
+            style={{
+              flex: 1.5,
+              // alignItems: 'center',
+              justifyContent: "flex-start",
+              // backgroundColor: 'red'
+            }}
+          >
+            <MaterialIcons
+              name="close"
+              size={29}
+              style={{
+                ...styles.modalToggle,
+                ...{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginTop: 22,
+                },
+              }}
+              onPress={() => setModalOpen(false)}
+            />
+            <Text style={styles.modalText}>{modalText}</Text>
+          </View>
+        </View>
+      </Modal>
+
+      <TouchableOpacity
+        onPress={() => {
+          setModalOpen(true);
+          setModalText(
+            "Kaltinimai, Įžeidinėjimai, Etikėčiu klijavimas, Kritika, Vertinimas."
+          );
+        }}
+      >
+        <Card title="Vertinimas">
+          <AntDesign
+            name={["dislike2", "like2"][Math.round(Math.random())]}
+            size={24}
+            color="black"
+          />
+        </Card>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          setModalOpen(true);
+          setModalText(
+            "Mes kiekvienas esame atsakingi už savo mintis, jausmus ir veiksmus."
+          );
+        }}
+      >
+        <Card title="Atsakomybės neprisiimimas">
+          <Entypo name="users" size={24} color="black" />
+        </Card>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => {
+          setModalOpen(true);
+          setModalText(
+            "Prielaida, kad tam tikri veiksmai nusipelno būti apdavanoti, o kiti nubausti."
+          );
+        }}
+      >
+        <Card title="Nuopelnais nukreipta kalba">
+          <MaterialCommunityIcons
+            name="diamond-stone"
+            size={24}
+            color="black"
+          />
+        </Card>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => {
+          setModalOpen(true);
+          setModalText(
+            "Prielaida, kad nėra kito pasirinkimo. Tokie žodžiai kaip: turi, privalai, būtina...."
+          );
+        }}
+      >
+        <Card title="Reikalavimai">
+          <FontAwesome5 name="hand-point-right" size={24} color="black" />
+        </Card>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function Log() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Log!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+export default function Modelis() {
+  return (
+    <NavigationContainer independent={true}>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="NVCModel"
+          component={NVCModel}
+          options={{
+            tabBarLabel: "NVC Modelis",
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign name="smileo" size={size} color="green" />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Blocks"
+          component={Blocks}
+          options={{
+            tabBarLabel: "Blokai",
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign name="frowno" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
